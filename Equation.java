50d99b02489750d78f91c4ee0693bf4b6cc1fc36
duong <isdon243@gmail.com>
2025-02-28 14:37:47 +0700

    second commit

--- Equation.java
+++ Equation.java
@@ -4,9 +4,9 @@ public class Equation {
     public static void main(String[] args) {
         double num1, num2, nghiem;
         String strnum1, strnum2, strNotification;
-        strnum1 = JOptionPane.showInputDialog(null, "Please input the first number:", 
+        strnum1 = JOptionPane.showInputDialog(null, "Please input the first number a in equation a*x + b = 0 :", 
         "Input the first number", JOptionPane.INFORMATION_MESSAGE);
-        strnum2 = JOptionPane.showInputDialog(null, "Please input the second number:", 
+        strnum2 = JOptionPane.showInputDialog(null, "Please input the second number b int equation a*x + b = 0:", 
         "Input the second number", JOptionPane.INFORMATION_MESSAGE);
         num1 = Double.parseDouble(strnum1);
         num2 = Double.parseDouble(strnum2);
@@ -14,11 +14,11 @@ public class Equation {
             if (num2 == 0) {
                 strNotification = "Infinite number of root.";
             } else {
-                strNotification = "Can't find .";
+                strNotification = "Can't find root";
             }
         } else {
             nghiem = -num2 / num1;   
-            strNotification = "Phuong trinh co nghiem x = " + nghiem;
+            strNotification = "Found  x = " + nghiem;
         }
         JOptionPane.showMessageDialog(null,  strNotification, "Display", 
             JOptionPane.INFORMATION_MESSAGE);
